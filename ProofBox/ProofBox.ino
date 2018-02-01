56c95b5e7536d6e252b1ac5d0b030f09ceb7acc5
Sindre S Brun <sindre.brun@outlook.com>
2018-02-01 08:23:51 +0100

    Add first version for proofbox controller

---
+++ ProofBox/ProofBox.ino
@@ -0,0 +1,107 @@
+#define BLYNK_PRINT Serial
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "e9181935ebf748b092567b7f19577de9";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "TP-LINK";
+char pass[] = "PandaBjorn";
+
+#define DHTPIN D2          // What digital pin we're connected to
+#define DHTTYPE DHT11     // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define ONE_WIRE_BUS D5
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+#define HEATERPIN D4
+
+BlynkTimer ambientTempTimer;
+BlynkTimer heaterTempTimer;
+BlynkTimer connectionTimer;
+BlynkTimer heaterTimer;
+BlynkTimer dataTimer;
+
+int heaterPower = LOW;
+float ambientTemp = 50;
+float heaterTemp = 50;
+
+void readAmbientSensor()
+{
+	ambientTemp = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+	if (isnan(ambientTemp)) {
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+	Serial.print("Temperature is: ");
+	Serial.println(ambientTemp);
+}
+
+void readDigitalThermometerTemp() {
+	sensors.requestTemperatures();
+	heaterTemp = sensors.getTempCByIndex(0);
+}
+
+void checkConnection()
+{
+	if (WiFi.status() != WL_CONNECTED) {
+		Blynk.begin(auth, ssid, pass);
+	}
+}
+
+void controlHeaterElement() {
+	if (ambientTemp < 28)
+	{
+		heaterPower = HIGH;
+	}
+	else {
+		heaterPower = LOW;
+	}
+	digitalWrite(HEATERPIN, heaterPower);
+	Serial.print("Heater is: ");
+	Serial.println(heaterPower);
+}
+
+void sendDataToBlynk() {
+	Blynk.virtualWrite(V1, ambientTemp);
+	Blynk.virtualWrite(V2, heaterPower);
+	Blynk.virtualWrite(V0, heaterTemp);
+}
+
+void setup() {
+	// Debug console
+	Serial.begin(9600);
+
+	Blynk.begin(auth, ssid, pass);
+
+	dht.begin();
+
+	pinMode(HEATERPIN, OUTPUT);
+
+	ambientTempTimer.setInterval(2000L, readAmbientSensor);
+	heaterTempTimer.setInterval(1000L, readDigitalThermometerTemp);
+	heaterTimer.setInterval(100L, controlHeaterElement);
+	connectionTimer.setInterval(10000L, checkConnection);
+	dataTimer.setInterval(200L, sendDataToBlynk);
+}
+
+void loop()
+{
+	Blynk.run();
+
+	ambientTempTimer.run();
+	heaterTimer.run();
+	connectionTimer.run();
+	dataTimer.run();
+}
